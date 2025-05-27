6341a79443d0e0b52d924ca568c7ea2e9d19d103
ozoz03 <oleg.zasymenko@gmail.com>
2025-05-27 19:10:19 +0300

    stack

---
+++ StackQueue/stack_constructor.py
@@ -0,0 +1,39 @@
+class Node:
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    #                               #
+    #                               #
+    #                               #
+    #                               #
+    #################################
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+            
+class Stack:
+    ## WRITE STACK CONSTRUCTOR HERE ##
+    #                                #
+    #                                #
+    #                                #
+    #                                #
+    ##################################
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+
+
+my_stack = Stack(4)
+
+print('Top:', my_stack.top.value)
+print('Height:', my_stack.height)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Top: 4
+    Height: 1
+
+"""
---
+++ StackQueue/stack_push.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    ### WRITE PUSH METHOD HERE ###
+    #                            #
+    #                            #
+    #                            #
+    #                            #
+    ##############################
+    def push(self, value):
+        new_node = Node(value=value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            temp = self.top
+            new_node.next = temp
+            self.top = new_node
+            
+        self.height += 1
+
+
+
+my_stack = Stack(2)
+
+print('Stack before push(1):')
+my_stack.print_stack()
+
+my_stack.push(1)
+
+print('\nStack after push(1):')
+my_stack.print_stack()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Stack before push(1):
+    2
+
+    Stack after push(1):
+    1
+    2   
+
+"""
